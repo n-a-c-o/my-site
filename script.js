581e78f7a99ee00321de17ecfe17be8d3115d114
Kanako_Shimizu <shimizukanaco@gmail.com>
2025-09-30 19:04:59 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// フッターの年号
+document.getElementById('year').textContent = new Date().getFullYear();
+
+// アンカーをなめらかスクロール
+document.querySelectorAll('a[href^="#"]').forEach(a=>{
+  a.addEventListener('click', e=>{
+    const id = a.getAttribute('href');
+    if(id.length > 1){
+      e.preventDefault();
+      document.querySelector(id)?.scrollIntoView({behavior:'smooth'});
+    }
+  });
+});
